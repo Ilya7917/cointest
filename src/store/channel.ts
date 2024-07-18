71227f7f24bd1211bc42f08701188faed3e3d339
John Doe <johndoe@email.com>
2024-07-19 00:53:05 +0700

    312

--- src/store/channel.ts
+++ src/store/channel.ts
@@ -11,9 +11,19 @@ export interface Channel {
     is_available: boolean;
 };
 
+export interface ChannelMember {
+    channelId: number;
+    userId: number;
+    status: string;
+    updatedAt: Date | null;
+    createdAt: Date | null;
+
+}
+
 export const useChannelsStore = defineStore('channels', {
     state: () => ({
         channels: null as Channel[] | null,
+        myChannels: null as ChannelMember[] | null,
     }),
     actions: {
         async fetchChannels() {
@@ -36,6 +46,8 @@ export const useChannelsStore = defineStore('channels', {
                     'x-api-key': userStore.getAccessToken,
                 },
             });
+            alert(response.data)
+            this.myChannels = response.data.channels
             return true;
         }
     },
